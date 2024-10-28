69519e78329339abb77d93f3f19083da041e4344
Mythily-29 <mythilyadckap.com>
2024-10-28 16:49:02 +0530

    code changed

---
+++ sample.js
@@ -0,0 +1,138 @@
+// first one
+
+// let n=prompt("Enter a number:")
+// for(i=1;i<=n;i++){
+    
+//     if(i%3==0 && i%5==0){
+//     console.log("FizzBuzz")
+//     }
+//     else if(i%3==0){
+//         console.log("Fizz")
+//     }
+//     else if(i%5==0){
+//         console.log("Buzz")
+//     }
+//     else{
+//         console.log(i)
+//     }
+// }
+
+// second one
+
+// let a=parseInt(prompt("Enter the number:"));
+// let b=parseInt(prompt("Enter the number:"));
+// let c=parseInt(prompt("Enter the number:"));
+// let d=parseInt(prompt("Enter the number:"));
+// let e=parseInt(prompt("Enter the number:"));
+// function sample(a,b,c,d,e){
+//     return a+b-c*d/e;
+// }
+// let result=sample(a,b,c,d,e);
+// console.log(result)
+
+// third one
+
+// let hour=parseInt(prompt("Enter the hours to convert:"))
+// function hourConvert(hour){
+//     return hour*60*60;
+// }
+// let result=hourConvert(hour);
+// console.log("Hour has been converted to seconds:",+result)
+
+// 4th one
+
+// let year=parseInt(prompt("Enter the years to convert:"))
+// function yearConvert(year){
+//     return year*365;
+// }
+// let result=yearConvert(year);
+// console.log("Year has been converted to seconds:",+result+ "days")
+
+// 5th one
+
+// let input=prompt("Enter a string:")
+// let vowel=['a','e','i','o','u']
+// // console.log(input.length)
+// let count=0;
+// for(i=0;i<input.length;i++){
+//     for(j=0;j<input.length;j++){
+//       if(input[i]==vowel[j]){
+//           count++;
+//           let result=input[i]
+//           console.log(result)
+//     } 
+//     }
+// }
+// console.log(count)
+
+
+// 6th one 
+
+// let input=parseInt(prompt("Enter a number:"))
+// for(i=1;i<input;i++){
+//     if(i%2==0){
+//         console.log(i,"- even")
+//     }
+//     else if(i!=0){
+//         console.log(i,"- odd")
+//     }
+//     else{
+//         console.log("Not an invalid input")
+//     }
+ 
+// }
+
+//7th one
+
+// let input1=prompt("Enter a strings:")
+// console.log(input1.length)
+// let spl=input1.split(" ")
+// console.log(spl)
+// for(i=spl.length-1;i>=0;i--){
+//     // console.log(spl[i])
+//     result=(spl[i].split(""))
+//     console.log(result)
+// }
+
+
+//8th one
+
+// let input1=12;
+// let input2="test";
+// if(input1===input2){
+//     console.log("Values and datatypes are equal")
+// }
+// else if(input1 == input2){
+//     console.log("values are equal but different datatype")
+// }
+// else{
+//     console.log("Invalid input")
+// }
+
+
+//9th one
+
+// let a=prompt("Enter a value:")
+// if(a>0){
+//     console.log("The given number is positive:" +a)
+// }
+// else if(a==0){
+//     console.log("The given number is false:" +a)
+// }
+// else if(a<0){
+//     console.log("The given number is negative:" +a)
+// }
+// else{
+//     console.log("The given number is invalid input:" +a)
+// }
+
+//10th one
+
+// let input1=true;
+// let input2="false";
+// if(input1 && input2)
+//     console.log("True")
+
+// else{
+//     console.log("invalid and undefined")
+// }
